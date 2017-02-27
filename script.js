835b25d9fe8c4a3b9650f55ba90f3fe8dcd03280
milezperhour <hyperbolvoid@gmail.com>
2017-02-27 13:45:19 -0800

    velocity and thrust works

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*
+* THE SHIP
+ */
+
 var ship;
 
 function setup(){
@@ -9,17 +13,22 @@ function draw(){
     background(0);
     ship.render();
     ship.turn();
+    ship.update();
+    ship.edges();
 }
 
 function keyReleased(){
     ship.setRotation(0);
+    ship.boosting(false);
 }
 
 function keyPressed(){
-    if (keyCode == RIGHT_ARROW){
+    if (keyCode === RIGHT_ARROW){
         ship.setRotation(0.1);
-    } else if (keyCode == LEFT_ARROW){
+    } else if (keyCode === LEFT_ARROW){
         ship.setRotation(-0.1);
+    } else if (keyCode === UP_ARROW){
+        ship.boosting(true);
     }
 }
 
@@ -28,17 +37,51 @@ function Ship(){
     this.r = 20;
     this.heading = 0;
     this.rotation = 0;
+    this.velocity = createVector(0, 0);
+    this.isBoosting = false;
+
+    this.boosting = function(boolean){
+        this.isBoosting = boolean;
+    };
+
+    this.update = function(){
+        if (this.isBoosting) {
+            this.boost();
+        }
+        this.pos.add(this.velocity);
+        this.velocity.mult(0.99);
+    };
+
+    this.boost = function(){
+        var force = p5.Vector.fromAngle(this.heading);
+        force.mult(0.1);
+        this.velocity.add(force);
+    };
 
     this.render = function(){
         translate(this.pos.x, this.pos.y);
-        rotate(this.heading);
+        rotate(this.heading + PI / 2);
         noFill();
         stroke(255);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
     };
 
-    this.setRotation = function(angle){
-        this.rotation = angle;
+    this.edges = function(){
+        if (this.pos.x > width + this.r){
+            this.pos.x = -this.r;
+        } else if (this.pos.x < -this.r){
+            this.pos.x = width + this.r;
+        }
+
+        if (this.pos.y > height + this.r){
+            this.pos.y = -this.r;
+        } else if (this.pos.y < -this.r){
+            this.pos.y = height + this.r;
+        }
+    };
+
+    this.setRotation = function(a){
+        this.rotation = a;
     };
 
     this.turn = function(){
