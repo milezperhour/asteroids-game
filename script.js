33872782de9ac88a88b2a3bc53e90cbfbd7973d5
milezperhour <hyperbolvoid@gmail.com>
2017-02-27 12:46:52 -0800

    ship turning left and right working

--- script.js
+++ script.js
@@ -8,13 +8,40 @@ function setup(){
 function draw(){
     background(0);
     ship.render();
+    ship.turn();
+}
+
+function keyReleased(){
+    ship.setRotation(0);
+}
+
+function keyPressed(){
+    if (keyCode == RIGHT_ARROW){
+        ship.setRotation(0.1);
+    } else if (keyCode == LEFT_ARROW){
+        ship.setRotation(-0.1);
+    }
 }
 
 function Ship(){
     this.pos = createVector(width/2, height/2);
-    this.r = 10;
+    this.r = 20;
+    this.heading = 0;
+    this.rotation = 0;
+
     this.render = function(){
         translate(this.pos.x, this.pos.y);
+        rotate(this.heading);
+        noFill();
+        stroke(255);
         triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+    };
+
+    this.setRotation = function(angle){
+        this.rotation = angle;
+    };
+
+    this.turn = function(){
+        this.heading += this.rotation;
     }
 }
